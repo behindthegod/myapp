ac4fe194d9487a8904050be196982bd6a5422bae
Ilya Karlyuk <divizionby0@gmail.com>
2020-02-24 10:48:35 +0300

    dialogs page update

--- sn/src/components/Dialogs/Dialogs.jsx
+++ sn/src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import classes from './Dialogs.module.css';
 
 const Dialogs = () => {
-    return <div>Dialogs</div>
-};
+    return <div className={classes.dialogs}>
+        <div className={classes.dialogItem}>
+            <div className={classes.dialog + ' ' + classes.active}>Sveta</div>
+            <div className={classes.dialog}>Sveta</div>
+        </div>
+            <div>
+                <div className={classes.message}> привет</div>
+                <div className={classes.message}> samuray</div>
+                <div className={classes.message}> js</div>
+            </div>
+        </div>
+
+        };
 
 export default Dialogs;
--- sn/src/components/Profile/MyPosts/Post/Post.jsx
+++ sn/src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,8 +5,7 @@ import React from "react";
 const Post = (props) => {
     return <div className={classes.post}>
         <img src="https://i04.fotocdn.net/s124/d6202488bbb86446/user_l/2821886434.jpg" alt='kot'/>
-        <div>{props.message}</div>
-        <div><span>like</span></div>
+        <div>{props.message} <span>like</span></div>
     </div>
 
 };
