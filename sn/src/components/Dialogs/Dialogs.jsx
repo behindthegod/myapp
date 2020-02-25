45a94062acb4578b3d6f4d45c3402c9b8438359b
Ilya Karlyuk <divizionby0@gmail.com>
2020-02-25 19:58:46 +0300

    add render=

--- sn/src/components/Dialogs/Dialogs.jsx
+++ sn/src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import classes from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 
-const DialogsItem = (props) => {
-    let path = "/dialogs/" + props.id;
-    return <div className={classes.dialog + ' ' + classes.active}>
-        <NavLink to={path}>{props.name}</NavLink>
-    </div>
-};
+
+
+
 
 let DialogsData = [
     {id: 1, name : "Sveta"},
@@ -25,16 +23,14 @@ let MessagesData = [
 
 
 
-const Message = (props) => {
-    return <div className={classes.message}>{props.message}</div>
-};
+
 
 const Dialogs = (props) => {
-    let dialogsElement = DialogsData.map( (d) => <DialogsItem name={d.name} id={d.id}/>);
+    let dialogsElement = DialogsData.map( (d) => <DialogItem name={d.name} id={d.id}/>);
     let messageElement = MessagesData.map( (m) => <Message message={m.message}/>);
 
     return <div className={classes.dialogs}>
-        <div className={classes.dialogItem}>
+        <div className={classes.DialogItem}>
             {dialogsElement}
         </div>
         <div>
