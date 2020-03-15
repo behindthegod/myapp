d5df34277f913b76678cc3ec1419bcbd3330ab19
Ilya Karlyuk <divizionby0@gmail.com>
2020-03-15 21:48:37 +0300

    58 up

--- sn/src/components/Users/UsersContainer.jsx
+++ sn/src/components/Users/UsersContainer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {
-    followAC,
-    setCurrentPageAC,
-    setUsersAC,
-    setUsersTotalCountAC,
-    toggleIsFetchingAC,
-    unfollowAC
+    follow,
+    setCurrentPage,
+    setUsers,
+    setTotalUsersCount,
+    toggleIsFetching,
+    unfollow
 } from '../Redux/users-reducer';
 import * as axios from 'axios';
 import Users from './Users';
@@ -57,29 +57,23 @@ let mapStateToProps = (state) => {
     }
 };
 
-let mapDispatchToProps = (dispatch) => {
-    return {
-        follow: (userId) => {
-            dispatch(followAC(userId));
-        },
-        unfollow: (userId) => {
-            dispatch(unfollowAC(userId));
-        },
-        setUsers: (users) => {
-            dispatch(setUsersAC(users));
-        },
-        setCurrentPage: (pageNumber) => {
-            dispatch(setCurrentPageAC(pageNumber));
-        },
-        setTotalUsersCount: (totalCount) => {
-            dispatch(setUsersTotalCountAC(totalCount));
-        },
-        toggleIsFetching: (isFetching) => {
-            dispatch(toggleIsFetchingAC(isFetching));
-        }
-
-    }
-};
+// let mapDispatchToProps = (dispatch) => {
+//     return {
+//         follow: followAC,
+//         unfollow: unfollowAC,
+//         setUsers: setUsersAC,
+//         setCurrentPage: setCurrentPageAC,
+//         setTotalUsersCount: setUsersTotalCountAC,
+//         toggleIsFetching: toggleIsFetchingAC
+//     }
+// };
 
 
-export default connect(mapStateToProps, mapDispatchToProps ) (UsersContainer);
+export default connect(mapStateToProps, {
+    follow,
+    unfollow,
+    setUsers,
+    setCurrentPage,
+    setTotalUsersCount,
+    toggleIsFetching
+} ) (UsersContainer);
